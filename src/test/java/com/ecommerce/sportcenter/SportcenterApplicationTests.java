adf4621cde3ee2c7137b6a98ff3bdff14805fb8d
Gopal Sakhwala <Gopal.Sakhwala@einfochips.com>
2024-04-12 23:18:51 +0530

    Initial Commit

---
+++ src/test/java/com/ecommerce/sportcenter/SportcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sportcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
